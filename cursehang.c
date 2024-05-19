d2c41326ce2473a029716b51735599634a46bcc6
Alister Brumley <ali.b912@gmail.com>
2024-05-19 18:54:47 +1000

    drawing basic gallows

--- cursehang.c
+++ cursehang.c
@@ -4,25 +4,27 @@
 
 int main(void) {
 	char line = '-';
-	int lineInt = 196;
-	//int wLen = strlen(word);
+	// int lineInt = 196;
+	// int wLen = strlen(word);
 	// DO NOT WRITE PAST 23, 79
 	initscr();
 	cbreak();
 	noecho();
 
-	printw("lmao");
-	move(5, 5);
-	//hline(line, 30);
-	WINDOW *win = newwin(10,10,1,1);
-	box(win, '*', '*');
-    touchwin(win);
-    wrefresh(win);
+	// printw("lmao");
+	move(2, 25);
+	addch(ACS_ULCORNER);
+	hline(ACS_HLINE, 20);
+	mvvline(3, 25, ACS_VLINE, 10);
+	move(13, 22);
+	hline(ACS_HLINE, 3);
+	mvaddch(13, 25, ACS_BTEE);
+	hline(ACS_HLINE, 25);
 	getch();
-
 	clear();
 	mvaddstr(0, 0, "exiting");
 	getch();
 	
 	endwin();
+	return 0;
 }
